8d85f65c8d9ee7c1cbf17ee8d2ca57cc59950423
Mateus Junges <mateus@junges.dev>
2024-03-16 12:19:42 -0300

    wip

--- src/MarkdownEditorServiceProvider.php
+++ src/MarkdownEditorServiceProvider.php
@@ -3,6 +3,9 @@
 namespace Spatie\FilamentMarkdownEditor;
 
 use Filament\Facades\Filament;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
@@ -18,14 +21,22 @@ class MarkdownEditorServiceProvider extends PackageServiceProvider
 
     public function bootingPackage()
     {
-        Filament::registerScripts([
-            'spatie-markdown-editor' => __DIR__.'/../resources/dist/editor.js',
-        ], true);
+        if (class_exists(FilamentAsset::class)) {
+            FilamentAsset::register([
+                Js::make('spatie-markdown-editor', __DIR__.'/../resources/dist/editor.js'),
+                Css::make('font-awesome', 'https://pro.fontawesome.com/releases/v5.15.4/css/all.css'),
+                Css::make('spatie-markdown-editor', __DIR__.'/../resources/css/editor.css')
+            ]);
+        } else {
+            Filament::registerScripts([
+                'spatie-markdown-editor' => __DIR__.'/../resources/dist/editor.js',
+            ], true);
 
-        Filament::registerStyles([
-            'https://pro.fontawesome.com/releases/v5.15.4/css/all.css',
-            'https://cdn.jsdelivr.net/npm/easymde/dist/easymde.min.css',
-            'spatie-markdown-editor' => __DIR__.'/../resources/css/editor.css',
-        ]);
+            Filament::registerStyles([
+                'https://pro.fontawesome.com/releases/v5.15.4/css/all.css',
+                'https://cdn.jsdelivr.net/npm/easymde/dist/easymde.min.css',
+                'spatie-markdown-editor' => __DIR__.'/../resources/css/editor.css',
+            ]);
+        }
     }
 }
